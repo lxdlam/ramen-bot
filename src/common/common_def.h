62fc7825569d6bd5e4254ddad0b08f4d3f7bcc69
lxdlam <lxdlam@gmail.com>
2020-03-10 14:39:33 +0800

    Fix: Full adapt to {fmt}

--- src/common/common_def.h
+++ src/common/common_def.h
@@ -5,7 +5,6 @@
 
 #include <algorithm>   // std::max
 #include <cinttypes>   // int64_t, PRI64d...
-#include <cstdio>      // std::printf
 #include <filesystem>  // std::filesystem family
 #include <memory>      // std::shared_ptr, std::unique_ptr, etc.
 #include <optional>    // std::optional, std::nullopt
